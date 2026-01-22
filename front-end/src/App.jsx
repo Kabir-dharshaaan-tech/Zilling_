e92f5b07a6689c8208b584460a9448b2e53ce37e
kabir-dharshaaan-tech <dharshaankabir@gmail.com>
2026-01-22 14:17:17 +0530

    done

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen w-full bg-black text-white">
+      {/* Navbar */}
+      <Navbar />
       <Home />
+      
+
+      
     </div>
   );
-}
+};
 
 export default App;
---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Navbar = () => {
+  const menuItems = [
+    "INDEX",
+    "OBSERVATORY",
+    "EXPEDITION",
+    "TRACES",
+    "CONTACT",
+  ];
+
+  return (
+    <div className="w-full flex justify-center mt-8">
+      {/* Outer dark frame */}
+      <div
+        className="
+          bg-black
+          px-2 py-2
+          shadow-xl
+        "
+        style={{
+          clipPath:
+            "polygon(3% 0%, 97% 0%, 100% 50%, 97% 100%, 3% 100%, 0% 50%)",
+        }}
+      >
+        {/* Inner orange bar */}
+        <nav
+          className="
+            flex items-center
+            bg-orange-500
+            overflow-hidden
+          "
+          style={{
+            clipPath:
+              "polygon(3% 0%, 97% 0%, 100% 50%, 97% 100%, 3% 100%, 0% 50%)",
+          }}
+        >
+          {menuItems.map((item, index) => (
+            <div key={item} className="relative">
+              <button
+                className="
+                  px-8 py-3
+                  text-black font-semibold
+                  tracking-wider
+                  hover:bg-orange-400
+                  transition-colors duration-200
+                "
+              >
+                {item}
+              </button>
+
+              {/* Divider */}
+              {index !== menuItems.length - 1 && (
+                <span className="absolute right-0 top-1/2 -translate-y-1/2 h-6 w-px bg-black/50" />
+              )}
+            </div>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
